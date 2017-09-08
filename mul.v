816f4c961ef4e856f3fee1b361d45d3de41cd553
stealth07 <andres07@email.arizona.edu>
2017-09-08 13:43:02 -0700

    Update 1: Added all verilog template files for assignment

---
+++ mul.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/08/2017 01:41:41 PM
+// Design Name: 
+// Module Name: mul
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mul(
+
+    );
+endmodule
---
+++ sub.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/08/2017 01:34:35 PM
+// Design Name: 
+// Module Name: sub
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sub(
+
+    );
+endmodule
