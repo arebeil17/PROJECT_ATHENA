4033c960394286555c328bfea524f94b555a95df
nramohalli <nramohalli@email.arizona.edu>
2017-09-24 08:53:37 -0700

    commented out my circuit
    Sorry it took so long. Busy weekend. the two versions of the circuit are
    up mine and whoever got to it first :)

--- circuit3.v
+++ circuit3.v
@@ -3,15 +3,15 @@
 // Company: 
 // Engineer: 
 // 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 // Create Date: 09/20/2017 06:24:28 PM
 // Design Name: 
 // Module Name: 574_circuit3
-=======
+//=======
 // Create Date: 09/08/2017 01:41:41 PM
 // Design Name: 
 // Module Name: mod
->>>>>>> origin/master
+//>>>>>>> origin/master
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -22,34 +22,34 @@
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-<<<<<<< HEAD
+//<<<<<<< HEAD
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+// Nikitha's Circuit 3
+//module circuit3( a, b, c, d, e, f, g, h, sa, avg );
+//input [15:0] a,b,c,d,e,f,g,h;
+//input [7:0] sa;
 
-module circuit3( a, b, c, d, e, f, g, h, sa, avg );
-input [15:0] a,b,c,d,e,f,g,h;
-input [7:0] sa;
+//output [15:0] avg;
 
-output [15:0] avg;
+//wire [31:0] l00, l01, l02, l03, l10, l11, l12, l2div2, l2div4, l2div8;
 
-wire [31:0] l00, l01, l02, l03, l10, l11, l12, l2div2, l2div4, l2div8;
+//parameter bw32= 32;
+//add #(.DATAWIDTH(bw32)) add_1 (a,b, l00);
+//add #(.DATAWIDTH(bw32)) add_2 (c,d, l01);
+//add #(.DATAWIDTH(bw32)) add_3 (e,f, l02);
+//add #(.DATAWIDTH(bw32)) add_4 (g,h,l03);
+//add #(.DATAWIDTH(bw32)) add_5 (l00, l01, l10);
+//add #(.DATAWIDTH(bw32)) add_6 (l02, l03, l11);
+//add #(.DATAWIDTH(bw32)) add_7 (l10, l11, l2);
 
-parameter bw32= 32;
-add #(.DATAWIDTH(bw32)) add_1 (a,b, l00);
-add #(.DATAWIDTH(bw32)) add_2 (c,d, l01);
-add #(.DATAWIDTH(bw32)) add_3 (e,f, l02);
-add #(.DATAWIDTH(bw32)) add_4 (g,h,l03);
-add #(.DATAWIDTH(bw32)) add_5 (l00, l01, l10);
-add #(.DATAWIDTH(bw32)) add_6 (l02, l03, l11);
-add #(.DATAWIDTH(bw32)) add_7 (l10, l11, l2);
+//shr #(.DATAWIDTH(bw32)) shr_1(l2,sa,l2div2);
+//shr #(.DATAWIDTH(bw32)) shr_2(l2div2, sa, l2div4);
+//shr #(.DATAWIDTH(bw32)) shr_3(l2div4, sa, l2div8);
 
-shr #(.DATAWIDTH(bw32)) shr_1(l2,sa,l2div2);
-shr #(.DATAWIDTH(bw32)) shr_2(l2div2, sa, l2div4);
-shr #(.DATAWIDTH(bw32)) shr_3(l2div4, sa, l2div8);
-
-assign avg= l2div8;
-=======
+//assign avg= l2div8;
+//=======
 //
 //input Int64 a, b, c
 //
